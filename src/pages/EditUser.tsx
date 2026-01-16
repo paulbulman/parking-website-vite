729f55508f09fcb6c146cb35f92c2572328ce02a
Paul Bulman <paulbulman@gmail.com>
2026-01-16 20:12:40 +0000

    Import types using type-only imports

--- src/pages/EditUser.tsx
+++ src/pages/EditUser.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, FormEvent } from "react";
+import { useState, useEffect, type FormEvent } from "react";
 import { useNavigate, useParams } from "react-router";
 import { useUser } from "../hooks/api/queries/user";
 import { useEditUser } from "../hooks/api/mutations/editUser";
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, FormEvent } from "react";
+import { useState, useEffect, type FormEvent } from "react";
 import { useProfile } from "../hooks/api/queries/profile";
 import { useEditProfile } from "../hooks/api/mutations/editProfile";
 import { useUserClaims } from "../hooks/useUserClaims";
