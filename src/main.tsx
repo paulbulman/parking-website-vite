397362c404d62aff0f7b51575e2bc72a03f5d9e8
Paul Bulman <paulbulman@gmail.com>
2026-01-16 23:57:27 +0000

    Add Sentry error reporting

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
+import "./instrument";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import * as Sentry from "@sentry/react";
 import { Amplify } from "aws-amplify";
 import amplifyConfig from "./amplifyconfiguration";
 import { config as FontAwesome } from "@fortawesome/fontawesome-svg-core";
@@ -11,7 +13,18 @@ Amplify.configure(amplifyConfig);
 
 FontAwesome.autoAddCss = false;
 
-createRoot(document.getElementById("root")!).render(
+const root = createRoot(document.getElementById("root")!, {
+  // Callback called when an error is thrown and not caught by an ErrorBoundary.
+  onUncaughtError: Sentry.reactErrorHandler((error, errorInfo) => {
+    console.warn("Uncaught error", error, errorInfo.componentStack);
+  }),
+  // Callback called when React catches an error in an ErrorBoundary.
+  onCaughtError: Sentry.reactErrorHandler(),
+  // Callback called when React automatically recovers from errors.
+  onRecoverableError: Sentry.reactErrorHandler(),
+});
+
+root.render(
   <StrictMode>
     <App />
   </StrictMode>
