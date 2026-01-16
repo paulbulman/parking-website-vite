6ae04edc90d4780e86f1d4866e43f2468723f05d
Paul Bulman <paulbulman@gmail.com>
2026-01-16 23:03:14 +0000

    Prevent FontAwesome CSP errors

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Amplify } from 'aws-amplify'
 import amplifyConfig from './amplifyconfiguration'
+import { config as FontAwesome } from "@fortawesome/fontawesome-svg-core";
 import './index.css'
 import App from './App.tsx'
 
 Amplify.configure(amplifyConfig)
+
+FontAwesome.autoAddCss = false;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
