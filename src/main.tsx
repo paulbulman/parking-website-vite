a8a63275d9e178e16f46b56a630265f23818e409
Paul Bulman <paulbulman@gmail.com>
2026-01-16 23:15:42 +0000

    Import FontAwesome style

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Amplify } from 'aws-amplify'
-import amplifyConfig from './amplifyconfiguration'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Amplify } from "aws-amplify";
+import amplifyConfig from "./amplifyconfiguration";
 import { config as FontAwesome } from "@fortawesome/fontawesome-svg-core";
-import './index.css'
-import App from './App.tsx'
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import "./index.css";
+import App from "./App.tsx";
 
-Amplify.configure(amplifyConfig)
+Amplify.configure(amplifyConfig);
 
 FontAwesome.autoAddCss = false;
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
